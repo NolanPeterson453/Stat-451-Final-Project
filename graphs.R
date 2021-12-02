e8fde0066d8e0df6d59f9de2229eacc3dac54813
NolanPeterson453 <79056951+NolanPeterson453@users.noreply.github.com>
2021-12-01 19:25:22 -0600

    Updated Guide Importance Scores
    Updated Guide Importance Scores

--- graphs.R
+++ graphs.R
@@ -25,4 +25,38 @@ FSLPR_REGION <- ggplot(data = soc_data, aes(y = FSLPR,
                                                  "Mountain",
                                                  "Pacific"))
 
-FSLPR_REGION
+#FSLPR_REGION
+
+## Create graph of importance scores 
+
+library(extrafont)
+
+loadfonts(device="win")
+fonts()
+impr.scr <- read.table(file = "451ImportanceScores.txt", header = TRUE, sep ="")
+impr.scr$Score <- as.numeric(impr.scr$Score)
+scores.graph <- ggpubr::ggdotchart(impr.scr, x = "Variable", y = "Score",
+                                   color = "Type",
+                                   palette = c("#00AFBB", "#E7B800", "#FC4E07"), # Custom color palette
+                                   main = "Guide Importance Scores",
+                                   sorting = "descending",                    # Sort value in descending order
+                                   add = "segments",                             # Add segments from y = 0 to dots
+                                   rotate = TRUE,                                # Rotate vertically
+                                   # Order by groups
+                                   dot.size = 6,                                 # Large dot size
+                                   label = round(impr.scr$Score),
+                                   font.label = list(color = "white", size = 7,
+                                                     vjust = 0.5),               # Adjust label parameters
+                                   ggtheme = ggplot2::theme_bw()                         # ggplot2 theme
+)
+
+ggplot2::ggsave(
+  file = "scores_graph.png",
+  plot = scores.graph,
+  scale = 1,
+  width = 18,
+  height = 24,
+  units = "in",
+  dpi = 300,
+  limitsize = TRUE,
+)
